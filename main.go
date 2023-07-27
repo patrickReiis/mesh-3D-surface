1c4332bb04463b3941c6d17fa7e785ce35321c1f
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-07-27 14:29:54 -0300

    Basic server setup

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = "8080"
 
 func main() {
-	fmt.Println("Hello World")
+
+	http.HandleFunc("/", handleDisplaySurface)
+
+	log.Printf("Running on http://localhost:%s", port)
+	if err := http.ListenAndServe("0.0.0.0:"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleDisplaySurface(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Display Surface")
 }
