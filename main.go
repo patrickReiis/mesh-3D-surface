3049a50907490e312058db12cbfd79abdaf4819f
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2023-07-28 12:35:23 -0300

    Added default values

--- main.go
+++ main.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"net/http"
 )
+
+const (
+	defaultWidth, defaultHeight = 600, 320                          // canvas size in pixels
+	defaultCells                = 100                               // number of grid cells
+	defaultXyrange              = 30.0                              // axis ranges (-xyrange..+xyrange)
+	defaultXyscale              = defaultWidth / 2 / defaultXyrange // pixels per x or y unit
+	defaultZscale               = defaultHeight * 0.4               // pixels per z unit
+	defaultAngle                = math.Pi / 6                       // angle of x, y axes (=30°)
+	defaultPeaksColor           = "#ff0000"                         // Red color is the default color for peaks
+	defaultValleysColor         = "#0000ff"                         // Blue color is the default color for valleys
+)
+
+var sin30, cos30 = math.Sin(defaultAngle), math.Cos(defaultAngle) // sin(30°), cos(30°)
 
 const port = "8080"
 
